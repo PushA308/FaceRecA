670634e2e54a0a151d12f97a96a03095ddd834a3
Piyush Subhash Atram <piyusha947@gmail.com>
2019-02-21 06:00:18 +0000

    Delete r.php

--- r.php
+++
@@ -1,8 +0,0 @@
-<?php
-//$input=$_GET['searchbox'];
-//$file = fopen("test.txt","w");
-//fwrite($file,$input);
-$command = escapeshellarg('recognitionfinal.py');
-$output = shell_exec($command);
-header('Location:push.php');
-?>
